d7e06892dde5cc165d020f6b6e53670c9a5c4a18
dadunadze1 <dadunadze@gmail.com>
2026-05-04 08:55:53 +0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,5 @@
-// Firebase v10 ES Modules
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBF421H4mkNB9Ve_uJ8Ph6z4LrbxzKlrC4",
@@ -14,7 +10,5 @@ const firebaseConfig = {
   appId: "1:882036563594:web:c800b0f2bb6977a441d773"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
