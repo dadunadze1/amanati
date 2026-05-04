1491945ed05bee713f1d448cfd58af7e844cb728
dadunadze1 <dadunadze@gmail.com>
2026-05-04 08:34:00 +0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,21 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
+// Firebase v10 (ES Modules)
 
-// IMPORTANT:
-// Firebase Console -> Project settings -> General -> Your apps -> Web app config
-// const firebaseConfig = {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+
+// შენი Firebase config
+const firebaseConfig = {
   apiKey: "AIzaSyBF421H4mkNB9Ve_uJ8Ph6z4LrbxzKlrC4",
   authDomain: "amanatebi123-43963.firebaseapp.com",
   projectId: "amanatebi123-43963",
   storageBucket: "amanatebi123-43963.firebasestorage.app",
   messagingSenderId: "882036563594",
-  appId: "1:882036563594:web:45cb83f81121eb7941d773"
-};
+  appId: "1:882036563594:web:c800b0f2bb6977a441d773"
 };
 
+// ინიციალიზაცია
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
